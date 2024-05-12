8949c52a468e2f2c92722fdbc689cc5d64e6993e
Ioannis Monastiriotis <ioannismonastiriotis@gmail.com>
2024-05-12 22:23:43 +0300

    Add project files.

---
+++ EleniBlog/Data/EleniContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EleniBlog.Data
+{
+    public class EleniContext : DbContext
+    {
+        public DbSet<Recipe>? Recipes { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Ingredient>? Ingredients { get; set; }
+
+      public EleniContext(DbContextOptions<EleniContext> dbContextOptions) :base( dbContextOptions )
+        { 
+
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Ingredient>()
+        //        .HasOne(i => i.Recipe) // Ingredient has one Recipe
+        //        .WithMany(r => r.Ingredients) // Recipe has many Ingredients
+        //        .HasForeignKey(i => i.RecipeId) // Ingredient has RecipeId as foreign key
+        //        .OnDelete(DeleteBehavior.Cascade); // Cascade delete: if a Recipe is deleted, its Ingredients are also deleted
+        //}
+
+        public class Ingredient
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public string? Quantity { get; set; }
+
+            // Navigation properties
+            public int RecipeId { get; set; } // Foreign key
+            public Recipe? Recipe { get; set; } // Reference navigation property
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public decimal Price { get; set; }
+            public string? Description { get; set; }
+            public int StockQuantity { get; set; }
+        }
+
+        public class Recipe
+        {
+            public int Id { get; set; }
+            public string? Title { get; set; }
+            public string? Description { get; set; }
+            public string? ImageUrl { get; set; }
+            public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>(); // Navigation property
+        }
+    }
+}
---
+++ EleniBlog/Program.cs
@@ -0,0 +1,23 @@
+using EleniBlog;
+using EleniBlog.Data;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SqliteWasmHelper;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSqliteWasmDbContextFactory<EleniContext>(opt => 
+opt.UseSqlite("Data Source=mytaskdb.sqlite3"));
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
+await builder.Build().RunAsync();
